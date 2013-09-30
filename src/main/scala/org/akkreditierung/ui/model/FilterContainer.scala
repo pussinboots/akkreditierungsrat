8051304a42269ebe540a2767087cc2d034686f5b
pussinboots <pussinboots666@googlemail.com>
2013-09-30 12:37:47 +0200

    - refactoring and use inline function no if anymore

--- src/main/scala/org/akkreditierung/ui/model/FilterContainer.scala
+++ src/main/scala/org/akkreditierung/ui/model/FilterContainer.scala
@@ -2,35 +2,27 @@ package org.akkreditierung.ui.model
 
 import com.avaje.ebean.{Expr, ExpressionList, Query}
 import org.apache.wicket.markup.html.form.{FormComponent, TextField}
+import java.sql.Connection
+import scalikejdbc.ConnectionPool
 
 class FilterContainer(hochSchule: TextField[String], fach: TextField[String], abschluss: TextField[String], agentur: TextField[String], studienForm: TextField[String]) {
 
-  def getHochSchule: String = return s"%${hochSchule.getValue}"
-  def getFach: String = return s"%${fach.getValue}"
-  def getAbschluss: String = return s"%${abschluss.getValue}"
-  def getAgentur: String = return s"%${agentur.getValue}"
-  def getStudienForm: String = return s"%${studienForm.getValue}"
-
   def apply[T](query: Query[T]) {
     val where: ExpressionList[T] = query.where
-    if (isNotEmpty(hochSchule)) {
-      where.like("hochschule", getHochSchule)
-    }
-    if (isNotEmpty(fach)) {
-      where.like("fach", getFach)
-    }
-    if (isNotEmpty(abschluss)) {
-      where.like("abschluss", getAbschluss)
-    }
-    if (isNotEmpty(agentur)) {
-      where.and(Expr.eq("map.k", "von"), Expr.like("map.v", getAgentur))
-    }
-    if (isNotEmpty(studienForm)) {
-      where.and(Expr.eq("map.k", "Besondere Studienform"), Expr.like("map.v", getStudienForm))
-    }
+    filterIfNotEmpty(hochSchule, value => where.like("hochschule", value))
+    filterIfNotEmpty(fach, value => where.like("fach", value))
+    filterIfNotEmpty(abschluss, value => where.like("abschluss", value))
+    filterIfNotEmpty(agentur, value => where.and(Expr.eq("map.k", "von"), Expr.like("map.v", value)))
+    filterIfNotEmpty(studienForm, value =>  where.and(Expr.eq("map.k", "Besondere Studienform"), Expr.like("map.v", value)))
   }
 
   def isNotEmpty(field: FormComponent[String]) = {
     field.getValue != null && field.getValue.length > 0
+  }
+
+  def filterIfNotEmpty(field: FormComponent[String], filter: String => _) {
+    if(isNotEmpty(field)) {
+      filter(s"%${field.getValue}%")
+    }
   }
 }
