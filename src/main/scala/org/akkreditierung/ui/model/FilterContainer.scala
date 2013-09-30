f071f507f6566d775752baee3345d2790c4903a7
pussinboots <pussinboots666@googlemail.com>
2013-09-30 12:25:24 +0200

    - fixed bug also empty value was rounded with % and like query was performed with it

--- src/main/scala/org/akkreditierung/ui/model/FilterContainer.scala
+++ src/main/scala/org/akkreditierung/ui/model/FilterContainer.scala
@@ -1,7 +1,7 @@
 package org.akkreditierung.ui.model
 
 import com.avaje.ebean.{Expr, ExpressionList, Query}
-import org.apache.wicket.markup.html.form.TextField
+import org.apache.wicket.markup.html.form.{FormComponent, TextField}
 
 class FilterContainer(hochSchule: TextField[String], fach: TextField[String], abschluss: TextField[String], agentur: TextField[String], studienForm: TextField[String]) {
 
@@ -13,20 +13,24 @@ class FilterContainer(hochSchule: TextField[String], fach: TextField[String], ab
 
   def apply[T](query: Query[T]) {
     val where: ExpressionList[T] = query.where
-    if (getHochSchule != null && getHochSchule.length > 0) {
+    if (isNotEmpty(hochSchule)) {
       where.like("hochschule", getHochSchule)
     }
-    if (getFach != null && getFach.length > 0) {
+    if (isNotEmpty(fach)) {
       where.like("fach", getFach)
     }
-    if (getAbschluss != null && getAbschluss.length > 0) {
+    if (isNotEmpty(abschluss)) {
       where.like("abschluss", getAbschluss)
     }
-    if (getAgentur != null && getAgentur.length > 0) {
+    if (isNotEmpty(agentur)) {
       where.and(Expr.eq("map.k", "von"), Expr.like("map.v", getAgentur))
     }
-    if (getStudienForm != null && getStudienForm.length > 0) {
+    if (isNotEmpty(studienForm)) {
       where.and(Expr.eq("map.k", "Besondere Studienform"), Expr.like("map.v", getStudienForm))
     }
+  }
+
+  def isNotEmpty(field: FormComponent[String]) = {
+    field.getValue != null && field.getValue.length > 0
   }
 }
