bbb3bf7352fd7d457fb84aad01abd127f3c5a797
pussinboots <pussinboots666@googlemail.com>
2013-11-07 13:55:02 +0100

    - set sort for fach field

--- src/main/scala/org/akkreditierung/ui/model/AdvertiserConfigModelProvider.scala
+++ src/main/scala/org/akkreditierung/ui/model/AdvertiserConfigModelProvider.scala
@@ -5,7 +5,7 @@ import org.apache.wicket.extensions.markup.html.repeater.data.sort.SortOrder
 
 @SerialVersionUID(-6117562733583734933L)
 class AdvertiserConfigModelProvider(filterContainer: FilterContainer) extends GenericProvider[AdvertiserConfig](new AdvertiserConfigBean) {
-  setSort("id", SortOrder.ASCENDING)
+  setSort("fach", SortOrder.ASCENDING)
 
   override def filter(query: Query[AdvertiserConfig]): Query[AdvertiserConfig] = {
     if (filterContainer != null) filterContainer.apply(query)
