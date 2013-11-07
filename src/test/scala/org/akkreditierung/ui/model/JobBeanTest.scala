4652738a0962518da431ed2910c9da1cbb6d5576
pussinboots <pussinboots666@googlemail.com>
2013-11-07 14:48:44 +0100

    - fixed bug findLatest Job returns not the latest one

--- src/test/scala/org/akkreditierung/ui/model/JobBeanTest.scala
+++ src/test/scala/org/akkreditierung/ui/model/JobBeanTest.scala
@@ -7,14 +7,15 @@ class JobBeanTest extends Specification with HSQLDbBefore {
 
   override def initTestData() {
     org.akkreditierung.model.Job.Insert(new org.akkreditierung.model.Job(newEntries=11))
+    org.akkreditierung.model.Job.Insert(new org.akkreditierung.model.Job(newEntries=1, status="finished"))
   }
 
   "JobBean" should {
     "return the latest job from database" in {
       val job = new JobBean().findLatest()
       org.akkreditierung.model.Job.Delete(new org.akkreditierung.model.Job(id=Option(job.id)))
-      job.newEntries must beEqualTo(11)
-      job.status must beEqualTo("started")
+      job.newEntries must beEqualTo(1)
+      job.status must beEqualTo("finished")
     }
   }
 }
