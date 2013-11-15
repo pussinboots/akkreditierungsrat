4b8007ef08203dfe2cd26d4081d4bda5d4c6873c
pussinboots <pussinboots666@googlemail.com>
2013-11-15 19:01:28 +0100

    - increased test coverage to nearly before 89 %

--- src/main/scala/org/akkreditierung/AkkreditierungsRatImport.scala
+++ src/main/scala/org/akkreditierung/AkkreditierungsRatImport.scala
@@ -13,15 +13,18 @@ object AkkreditierungsRatImport extends App {
 
   DB.WithSSL()
   DB.getMysqlConnection()
+  importStudienGaenge()
 
-  val sessionId = getSessionId()
-  println(s"Session ${sessionId}")
+  def importStudienGaenge(sessionId: String = getSessionId(), step:Int = 100, end: Int = 5100) {
 
-  //TODO actual the loop has to be defined with a specific end here 5100 studiengang is reached than break need a method to find when the table is at his end
-  fetchAndStoreStudienGaenge(sessionId, 100, 5100, {
-    studienGang: Studiengang =>
-      println(fetchAndStoreStudienGangInfo(sessionId, studienGang))
-  })
+    println(s"Session ${sessionId}")
+
+    //TODO actual the loop has to be defined with a specific end here 5100 studiengang is reached than break need a method to find when the table is at his end
+    fetchAndStoreStudienGaenge(sessionId, step, end, {
+      studienGang: Studiengang =>
+        println(fetchAndStoreStudienGangInfo(sessionId, studienGang))
+    })
+  }
 }
 
 object AkkreditierungsRatUpdate extends App {
@@ -31,33 +34,35 @@ object AkkreditierungsRatUpdate extends App {
 
   DB.WithSSL()
   DB.getMysqlConnection()
+  updateStudiengaenge()
 
-  val sessionId = getSessionId()
-  println(s"Session ${sessionId}")
+  def updateStudiengaenge(sessionId: String = getSessionId(), threadCount: Int = 4) {
+    println(s"Session ${sessionId}")
 
-  val studienGaenge = Studiengang.findAllNotUpdatedInLastSevenDays()
+    val studienGaenge = Studiengang.findAllNotUpdatedInLastSevenDays()
 
-  import scala.concurrent.duration._
-  import scala.concurrent._
-  implicit val ec = new ExecutionContext {
-    val threadPool = Executors.newFixedThreadPool(4);
-    def execute(runnable: Runnable) = threadPool.submit(runnable)
-    def reportFailure(t: Throwable) = t.printStackTrace()
+    import scala.concurrent.duration._
+    import scala.concurrent._
+    implicit val ec = new ExecutionContext {
+      val threadPool = Executors.newFixedThreadPool(threadCount);
+      def execute(runnable: Runnable) = threadPool.submit(runnable)
+      def reportFailure(t: Throwable) = t.printStackTrace()
+    }
+
+    val tasks: Seq[Future[Studiengang]] = for(studienGang <- studienGaenge) yield future {
+        val changed = fetchAndStoreStudienGangInfo(sessionId, studienGang, UpdateStudienGangAttribute)
+        if (changed) {
+          studienGang.modifiedDate = Some(new Date())
+          Studiengang.UpdateModifiedDate(studienGang)
+          println(s"Updated ${studienGang}")
+        }
+        Studiengang.UpdateUpdateDate(new Date(), studienGang)
+    }
+
+    val aggregated: Future[Seq[Studiengang]] = Future.sequence(tasks)
+
+    val squares: Seq[Studiengang] = Await.result(aggregated, Duration.Inf)
+    println("End updating")
   }
-
-  val tasks: Seq[Future[Studiengang]] = for(studienGang <- studienGaenge) yield future {
-      val changed = fetchAndStoreStudienGangInfo(sessionId, studienGang, UpdateStudienGangAttribute)
-      if (changed) {
-        studienGang.modifiedDate = Some(new Date())
-        Studiengang.UpdateModifiedDate(studienGang)
-        println(s"Updated ${studienGang}")
-      }
-      Studiengang.UpdateUpdateDate(new Date(), studienGang)
-  }
-
-  val aggregated: Future[Seq[Studiengang]] = Future.sequence(tasks)
-
-  val squares: Seq[Studiengang] = Await.result(aggregated, Duration.Inf)
-  println("End updating")
   sys.exit()
 }
--- src/test/scala/org/akkreditierung/AkkreditierungsRatImportSpec.scala
+++ src/test/scala/org/akkreditierung/AkkreditierungsRatImportSpec.scala
@@ -3,7 +3,7 @@ package org.akkreditierung
 import org.specs2.mutable._
 import org.akkreditierung.model._
 import org.akkreditierung.AkkreditierungsRatClient._
-import org.akkreditierung.test.HSQLDbBefore
+import org.akkreditierung.test.{NullableBodyMatcher, Betamax, HSQLDbBefore}
 import co.freeside.betamax.{MatchRule, TapeMode, Recorder}
 import co.freeside.betamax.proxy.jetty.ProxyServer
 import scala.Some
@@ -16,57 +16,22 @@ class AkkreditierungsRatImportSpec extends Specification with HSQLDbBefore {
 
   //activate betamax proxy for dispatch
 
-  class NullableBodyMatcher extends Comparator[Request] {
-    override def compare(a: Request, b: Request) = {
-      if (a.hasBody) {
-        val bodyA = scala.io.Source.fromInputStream(a.getBodyAsBinary).mkString
-        val bodyB = scala.io.Source.fromInputStream(b.getBodyAsBinary).mkString
-        bodyA.compareTo(bodyB)
-      } else
-        0
-    }
-  }
-
-  override def initTestData() {
-    sys.props.+=("com.ning.http.client.AsyncHttpClientConfig.useProxyProperties" -> "true")
-    val recorder = new Recorder
-    val proxyServer = new ProxyServer(recorder)
-    val mode: Option[TapeMode] = Some(TapeMode.READ_ONLY)
-    import collection.JavaConversions._
-    val list: java.util.List[Comparator[Request]] = Seq(MatchRule.method, MatchRule.uri, new NullableBodyMatcher())
-    recorder.insertTape("akkreditierungsratimport", Map("match" -> list))
-    recorder.getTape.setMode(mode.getOrElse(recorder.getDefaultMode()))
-    proxyServer.start()
-    try {
-      val sessionId = getSessionId()
-      fetchAndStoreStudienGaenge(sessionId, 30, 120, {
-        studienGang: Studiengang =>
-          fetchAndStoreStudienGangInfo(sessionId, studienGang)
-      })
-    } finally {
-      recorder.ejectTape()
-      proxyServer.stop()
-    }
-  }
-
-  "The AkkreditierungsRat Client" should {
-    "fetch studiengaenge from playback" in {
-      "fetch and store studiengaenge in the database" in {
-        initTestData()
+  "The AkkreditierungsRatImport Client" should {
+    "fetch studiengaenge from akkreditierungs database" in {
+      "fetch and store new studiengaenge in the database" in Betamax(tape="akkreditierungsratimport", list=Seq(MatchRule.method, MatchRule.uri, new NullableBodyMatcher())) {
+        AkkreditierungsRatImport.importStudienGaenge("2B2C176F125F48AF828FE2A8923FC461", 30, 120)
         Job.findLatest().get.newEntries must beEqualTo(239)
         Studiengang.findAll().length must beEqualTo(239)
       }
 
-//      "check that 1226 studiengang attribute are stored in the database for the 30 fetched studiengaenge" in {
-//        Job.findLatest().get.newEntries must beEqualTo(60)
-//        StudiengangAttribute.findAll().length must beEqualTo(1226)
-//        Studiengang.findByFach("Alternativer Tourismus").gutachtentLink.get must beEqualTo("http://www.aqas.de/downloads/Gutachten/49_319_BWL")
-//        Studiengang.findByFach("Altertumswissenschaften").gutachtentLink must beEqualTo(None)
-//        Studiengang.findByFach("Altertumswissenschaften").jobId must beEqualTo(Job.findLatest().get.id)
-//        val source = Source.FindByName("akkreditierungsrat")
-//        Studiengang.findByFach("Altertumswissenschaften").sourceId must beEqualTo(source.get.id.get)
-//        Studiengang.findByFach("Advanced Physical Methods in Radiotherapy").sourceId must beEqualTo(source.get.id.get)
-//      }
+
+      //TODO how to check update perform well
+      "update existing studiengaenge in the database" in Betamax(tape="akkreditierungsratimport", list=Seq(MatchRule.method, MatchRule.uri, new NullableBodyMatcher())) {
+        AkkreditierungsRatImport.importStudienGaenge("2B2C176F125F48AF828FE2A8923FC461", 30, 120)
+        AkkreditierungsRatUpdate.updateStudiengaenge("2B2C176F125F48AF828FE2A8923FC461", 1)
+        Job.findLatest().get.newEntries must beEqualTo(239)
+        Studiengang.findAll().length must beEqualTo(239)
+      }
     }
   }
 }
