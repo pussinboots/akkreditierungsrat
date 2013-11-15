453f47251dab5f2bf1dbfd7081d8880a612988d8
pussinboots <pussinboots666@googlemail.com>
2013-11-15 18:28:07 +0100

    - added tests for the new methods in the Models

--- src/test/scala/org/akkreditierung/model/ModelSpec.scala
+++ src/test/scala/org/akkreditierung/model/ModelSpec.scala
@@ -3,13 +3,24 @@ package org.akkreditierung.model
 import org.specs2.mutable._
 import org.akkreditierung.test.HSQLDbBefore
 import java.util.Date
+import anorm._
+import scala.Some
+import anorm.SqlParser._
+import anorm.~
+import scala.Some
 
 class ModelSpec extends Specification with HSQLDbBefore {
   sequential
 
   override def initTestData() {
-    Studiengang.Insert(new Studiengang(None, Some(1), "fach", "abschluss", "hochschule", "bezugstyp", Some("link"), None, Some(new Date()), 1))
-    Studiengang.Insert(new Studiengang(None, Some(2), "fach2", "abschluss2", "hochschule2", "bezugstyp2", Some("link"), Some("gutachtenlink"), Some(new Date()), 2))
+    val studienGang1 = Studiengang.Insert(new Studiengang(None, Some(1), "fach", "abschluss", "hochschule", "bezugstyp", Some("link"), None, Some(new Date()), 1))
+    StudiengangAttribute.Insert(new StudiengangAttribute(studienGang1.id.get, "fach", "fach"))
+    StudiengangAttribute.Insert(new StudiengangAttribute(studienGang1.id.get, "tel", "0123456789"))
+    StudiengangAttribute.Insert(new StudiengangAttribute(studienGang1.id.get, "www", "www.fach.de"))
+    val studienGang2 = Studiengang.Insert(new Studiengang(None, Some(2), "fach2", "abschluss2", "hochschule2", "bezugstyp2", Some("link"), Some("gutachtenlink"), Some(new Date()), 2))
+    StudiengangAttribute.Insert(new StudiengangAttribute(studienGang2.id.get, "fach", "fach2"))
+    StudiengangAttribute.Insert(new StudiengangAttribute(studienGang2.id.get, "tel", "9876543210"))
+    StudiengangAttribute.Insert(new StudiengangAttribute(studienGang2.id.get, "www", "www.fach2.de"))
   }
 
   "Studiengang class" should {
@@ -24,7 +35,43 @@ class ModelSpec extends Specification with HSQLDbBefore {
       Studiengang.findAll().length must beEqualTo(2)
     }
     "retrieve one studiengaenge by fach from database" in {
-      Studiengang.findByFach("fach2").fach must beEqualTo("fach2")
+      val studienGang = Studiengang.findByFach("fach2")
+      studienGang.fach must beEqualTo("fach2")
+      studienGang.studiengangAttributes.size must beEqualTo(3)
+      studienGang.studiengangAttributes.get("fach").get.value must beEqualTo("fach2")
+    }
+    "update updateDate field of one studienganag" in {
+      val studienGang = Studiengang.findByFach("fach")
+      val now = new Date()
+      Studiengang.UpdateUpdateDate(now, studienGang)
+      val updateDate = DB.withConnection {
+        implicit connection =>
+          SQL("select updateDate from studiengaenge where fach={fach}").on(
+            'fach -> studienGang.fach).single({get[Date]("studiengaenge.updateDate") map { case updateDate => updateDate}})
+      }
+
+      updateDate.getTime must beEqualTo(now.getTime)
+    }
+    "update modiefiedDate field of one studiengaenge" in {
+      val studienGang = Studiengang.findByFach("fach")
+      val now = new Date()
+      studienGang.modifiedDate=Some(now)
+      Studiengang.UpdateModifiedDate(studienGang)
+      val studienGangChanged = Studiengang.findByFach("fach")
+
+      studienGangChanged.modifiedDate.get.getTime must beEqualTo(now.getTime)
+    }
+  }
+
+  "StudiengangAttribute object" should {
+    "retrieve all StudiengangAttributes from database" in {
+      StudiengangAttribute.findAll().length must beEqualTo(6)
+    }
+    "retrieve one studiengaenge by fach from database" in {
+      val attribute = Studiengang.findByFach("fach2").studiengangAttributes.get("fach").get
+      attribute.value must beEqualTo("fach2")
+      StudiengangAttribute.Update(new StudiengangAttribute(id=attribute.id, key=attribute.key, value="fach2 erweitert"))
+      Studiengang.findByFach("fach2").studiengangAttributes.get("fach").get.value must beEqualTo("fach2 erweitert")
     }
   }
 
