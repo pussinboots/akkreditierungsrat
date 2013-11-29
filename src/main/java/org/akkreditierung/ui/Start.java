362c84996bc48fd57fd73eccffef2d3b43c5fd2b
pussinboots <pussinboots666@googlemail.com>
2013-11-29 13:37:13 +0100

    - activate SSL after the slick migration was lost

--- src/main/java/org/akkreditierung/ui/Start.java
+++ src/main/java/org/akkreditierung/ui/Start.java
@@ -1,17 +1,13 @@
 package org.akkreditierung.ui;
 
-import com.avaje.ebean.EbeanServerFactory;
-import com.avaje.ebean.Transaction;
-import com.avaje.ebean.config.DataSourceConfig;
-import com.avaje.ebean.config.ServerConfig;
 import org.akkreditierung.model.DB;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
-import scala.Tuple3;
 import scala.util.Properties;
 
 public class Start {
 	public static void main(String[] args) throws Exception {
+        DB.WithSSL();
         String webappDirLocation = "src/main/webapp/";
         int port = Integer.valueOf(Properties.envOrElse("PORT", "8081"));
 
