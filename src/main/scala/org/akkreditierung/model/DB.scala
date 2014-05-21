f0b5d2d32724e5688dcb7e1115ccdb4496613367
pussinboots <pussinboots666@googlemail.com>
2014-05-21 15:59:15 +0200

    - added database creation script based on the slick schema definition

--- src/main/scala/org/akkreditierung/model/DB.scala
+++ src/main/scala/org/akkreditierung/model/DB.scala
@@ -26,7 +26,7 @@ object DB {
   }
 
   def dbConfigUrl: String = {
-    val p = Properties.envOrElse("CLEARDB_DATABASE_URL", "mysql://root:root@127.0.0.1:3306/heroku_9852f75c8ae3ea1")
+    val p = Properties.envOrElse("CLEARDB_DATABASE_URL", "mysql://root:mysql@127.0.0.1:3306/heroku_9852f75c8ae3ea1")
     println(p)
     p
   }
