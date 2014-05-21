7b21ee3a5f40077b8458b83a4c9e46f0ffbecdfc
pussinboots <pussinboots666@googlemail.com>
2014-05-21 15:50:51 +0200

    - clean up

--- scripts/db/delete_studiengaenge_master.sql
+++
@@ -1,6 +0,0 @@
-use `heroku_9852f75c8ae3ea1`;
-DELETE 	del,
-	    sel
-FROM 	`heroku_9852f75c8ae3ea1`.`studiengaenge` as del
-JOIN 	`heroku_9852f75c8ae3ea1`.`studiengaenge_attribute` as sel
-WHERE del.abschluss like "%master%" and sel.id = del.id
